e4de1a521a126246bd3d46b46013aae9901985e4
Nikita Kazakevich <nikita.kazakevich@planner5d.com>
2023-02-19 21:10:54 +0300

    unit-tests for CommandBuilder

--- src/__tests__/CommandBuilder.test.ts
+++ src/__tests__/CommandBuilder.test.ts
@@ -1,4 +1,5 @@
 import type * as CommandBuilderTypes from "../core/command/CommandBuilder";
+import { CommandOperator } from "../core/command/CommandOperator";
 
 const { CommandBuilder } = jest.requireActual<typeof CommandBuilderTypes>("../core/command/CommandBuilder");
 
@@ -15,6 +16,42 @@ describe("Command builder test suite", () => {
       commandBuilder.setName(value);
       expect(commandBuilder.getName()).toBe(value);
     });
+  });
+
+  test("Build correct param object without operators", () => {
+    const commandName = "testCommand";
+    const simpleOption = "-i";
+    const simpleValue = "path/test/item";
+    commandBuilder.setName(commandName);
+    commandBuilder.setOption(simpleOption, simpleValue);
+
+    let buildResult = commandBuilder.build();
+    expect(buildResult.name).toBe(commandName);
+    expect(buildResult.params).toEqual([simpleOption, simpleValue]);
+
+    const optionWithNoOperator = "--flagWithNoOperator";
+    commandBuilder.setOption(optionWithNoOperator);
+    buildResult = commandBuilder.build();
+    expect(buildResult.name).toBe(commandName);
+    expect(buildResult.params).toEqual([simpleOption, simpleValue, optionWithNoOperator]);
+  });
+
+  test("Build correct param object with equal operator", () => {
+    const optionWithEqualOperator = "-equalOption";
+    const valueWithEqualOperator = "equalValue";
+    commandBuilder.setOption(optionWithEqualOperator, valueWithEqualOperator, CommandOperator.Equal);
+    expect(commandBuilder.build().params).toEqual([
+      optionWithEqualOperator + CommandOperator.Equal + valueWithEqualOperator,
+    ]);
+  });
+
+  test("Build correct param object with colon operator", () => {
+    const optionWithEqualOperator = "colonOption";
+    const valueWithEqualOperator = "colonValue";
+    commandBuilder.setOption(optionWithEqualOperator, valueWithEqualOperator, CommandOperator.Colon);
+    expect(commandBuilder.build().params).toEqual([
+      optionWithEqualOperator + CommandOperator.Colon + valueWithEqualOperator,
+    ]);
   });
 });
 
